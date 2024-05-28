e8d80722425cb3a82612333747bcc29ae7d4cd57
John Diego <johnoliveirasilva9@gmail.com>
2024-05-28 19:38:42 -0300

    variables to

---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "project" {
+  default = "cloud-deploy-424319"
+}
+variable "credentials_file" {
+  default = "credentials.json"
+}
+
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-c" 
+}
+
+variable "machine_type" {
+  default = "f1-micro"
+}
+
+variable "os_image" {
+  default = "ubuntu-os-cloud/ubuntu-2204-lts"
+}
+
+
+variable "gce_ssh_private_key_file" {
+  default = "~/.ssh/gcp_key"
+}
+
+variable "gce_ssh_pub_key_file" {
+  default = "~/.ssh/gcp_key.pub"
+}
+variable "user" {
+  default = "johnoliveirasilva9"
+}
+
+variable "size" {
+  default = 20
+}
